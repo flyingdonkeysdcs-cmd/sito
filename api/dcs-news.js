c2c6dd974321d75b8526e6b49f720c01047aece6
flyingdonkeysdcs-cmd <flyingdonkeysdcs@gmail.com>
2026-05-18 15:13:23 +0200

    Add files via upload

--- api/dcs-news.js
+++ api/dcs-news.js
@@ -85,9 +85,9 @@ export default async function handler(req, res) {
     );
 
     res.setHeader(
-      "Cache-Control",
-      "s-maxage=21600, stale-while-revalidate=86400"
-    );
+		"Cache-Control",
+		"s-maxage=86400, stale-while-revalidate=172800"
+		);
 
     res.status(200).json(news);
   } catch (error) {
